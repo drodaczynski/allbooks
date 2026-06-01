e584a1622842f703a26610fe717c5722d6326a22
Daniel Rodaczynski <drodaczynski@yahoo.com.br>
2026-06-01 16:23:03 -0300

    Merge from repository

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do allbooks")
-  console.log("API disponível através da url http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 572f61427a168cfaf6b6f1c70934f82c48c1c1b7
 })
